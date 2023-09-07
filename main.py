cfdf384a83e1356c798e89a87dd9e5ec9c8a75c0
Navarrisius <143776916+Navarrisius@users.noreply.github.com>
2023-09-06 23:12:46 -0300

    Add files via upload
    Terreno generado con Noise

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import noise
 import random
 import math
-from clases import partida
+import clases
 
 def main():
     ANCHO_VENTANA = 1280
@@ -11,10 +11,8 @@ def main():
     ANCHO_MUNDO = math.ceil(ANCHO_VENTANA/1.5)
     ALTURA_MUNDO = math.ceil(ALTO_VENTANA/1.5)
     pantalla = pygame.display.set_mode((ANCHO_VENTANA, ALTO_VENTANA), pygame.RESIZABLE,pygame.OPENGL)
-    game = partida.Partida()
-    print(ANCHO_MUNDO)
-    print(ALTURA_MUNDO)
-    textura_terreno = game.generar_terreno(ANCHO_MUNDO,ALTURA_MUNDO)
+    game = clases.Partida()
+    textura_terreno = game.generar_terreno(ANCHO_MUNDO, ALTURA_MUNDO)
     running = game.en_partida
     reloj = pygame.time.Clock()
 
@@ -23,14 +21,6 @@ def main():
     pygame.display.set_caption(NOMBRE_VENTANA)
 
     while running:
-        # Limita los FPS a 60
-        reloj.tick(60) 
-
-        pantalla.fill((255, 255, 255))
-
-        game.cargar_fondo(pantalla)
-
-        pygame.display.flip()
         # Captura todos los eventos dentro del juego
         for event in pygame.event.get():
             # Captura el cierre de la ventana
@@ -39,13 +29,14 @@ def main():
             elif event.type == pygame.VIDEORESIZE: # Captura eventos de cambio de tamaño de ventana
                 NUEVO_ANCHO, NUEVA_ALTURA = event.size
                 pantalla = pygame.display.set_mode((NUEVO_ANCHO, NUEVA_ALTURA), pygame.RESIZABLE,pygame.OPENGL)# Cambia el tamaño de la ventana
-                
+
         terreno_escalado = pygame.transform.scale(textura_terreno, (pantalla.get_width(), pantalla.get_height()))
-        
         pantalla.blit(terreno_escalado, (0, 0))
-        
         pygame.display.flip()
-        
+
+        # Limita los FPS a 60
+        reloj.tick(60) 
+
     pygame.quit()
-    
+
 main()
