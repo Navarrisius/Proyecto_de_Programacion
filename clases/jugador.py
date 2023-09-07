f8fa2297668b793e377ab0e3483d65411afa31a0
Navarrisius <143776916+Navarrisius@users.noreply.github.com>
2023-09-06 23:14:44 -0300

    Carpeta de clases traspasada a un archivo con todas las clases

--- clases/jugador.py
+++
@@ -1,8 +0,0 @@
-class Jugador:
-    nombre = None
-    tanque = None
-    puede_jugar = False
-
-    def __init__(self, nombre, tanque):
-        self.nombre = nombre
-        self.tanque = tanque
--- clases/tanque.py
+++
@@ -1,8 +0,0 @@
-class Tanque:
-    color = None
-    posicion_x = None
-    posicion_y = None
-    vivo = True
-
-    def __init__(self, color):
-        self.color = color
