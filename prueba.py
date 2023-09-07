ddddeda8536cfcdd203de74ddff20ab021a2fb87
Raulenho <rbaeza0316@gmail.com>
2023-09-06 21:19:57 -0300

    una mejora al terreno

--- prueba.py
+++ prueba.py
@@ -1,29 +1,31 @@
 import pygame
 import noise
 import random
+import math
 
 pygame.init()
 
-ANCHO_MUNDO = 1280
-ALTURA_MUNDO = 768
+ANCHO = 1280
+ALTURA = 768
 ESCALA_RUIDO = 0.01
 COLOR_TERRENO = (128, 64, 0)
-semilla = random.randint(0, 100)
+semilla = random.randint(0, 50)
 def generar_terreno(ANCHO_MUNDO,ALTURA_MUNDO):
 
     terreno = pygame.Surface((ANCHO_MUNDO, ALTURA_MUNDO))
 
     for x in range(ANCHO_MUNDO):
         # Determina la altura del terreno en este punto, el primer decimal para aumentar la altura de las montañas , el segundo para aumentar o disminuir el terreno
-        altura = int(noise.pnoise1(x * ESCALA_RUIDO, base=semilla) * 0.5 * ALTURA_MUNDO + 0.5 * ALTURA_MUNDO)
+        altura = int(noise.pnoise1(x * ESCALA_RUIDO, base=semilla) * 0.3 * ALTURA_MUNDO + 0.5 * ALTURA_MUNDO)
         # Rellena el terreno hasta esta altura
         pygame.draw.line(terreno, COLOR_TERRENO, (x, ALTURA_MUNDO), (x, altura), 1)
 
     return terreno
 
 def main():
-
-    screen = pygame.display.set_mode((ANCHO_MUNDO, ALTURA_MUNDO), pygame.RESIZABLE)
+    ANCHO_MUNDO = math.ceil(ANCHO/1.5)
+    ALTURA_MUNDO = math.ceil(ALTURA/1.5)
+    screen = pygame.display.set_mode((ANCHO, ALTURA), pygame.RESIZABLE,pygame.OPENGL)
     pygame.display.set_caption("Juego xd")
     textura_terreno = generar_terreno(ANCHO_MUNDO, ALTURA_MUNDO)
 
@@ -35,12 +37,9 @@ def main():
             elif event.type == pygame.VIDEORESIZE:  # Captura eventos de cambio de tamaño de ventana
                 new_width, new_height = event.size
                 screen = pygame.display.set_mode((new_width, new_height), pygame.RESIZABLE)  # Cambia el tamaño de la ventana
-                textura_terreno = generar_terreno(new_width, new_height)  # Regenera el terreno con el nuevo tamaño
 
         terreno_escalado = pygame.transform.scale(textura_terreno, (screen.get_width(), screen.get_height()))
-
-        #terreno_escalado = pygame.transform.scale(textura_terreno, (ANCHO_MUNDO , ALTURA_MUNDO))
-
+        
         screen.blit(terreno_escalado, (0, 0))
         pygame.display.flip()
 
