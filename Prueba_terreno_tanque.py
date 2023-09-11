f25193e5692896446493a0e9a5c46f947b9dcd5b
mario <mario.jorquera.11@gmail.com>
2023-09-10 23:25:23 -0300

    se agregó cañon trabajado con sen-cos

--- Prueba_terreno_tanque.py
+++ Prueba_terreno_tanque.py
@@ -21,9 +21,12 @@ def main():
     pygame.display.set_caption(NOMBRE_VENTANA)
 
     #Seting tanque
+    tanque_1 = clases.Tanque("red")
     gravedad = 1
-    tanque_x = 30
-    tanque_y = 30
+    tanque_1.posicion_x = 30
+    tanque_1.posicion_y = 30
+    radio = 30
+    angulo_n = 0
 
     while running:
         # Captura todos los eventos dentro del juego
@@ -31,22 +34,33 @@ def main():
             # Captura el cierre de la ventana
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.VIDEORESIZE: # Captura eventos de cambio de tamaño de ventana
+            if event.type == pygame.VIDEORESIZE: # Captura eventos de cambio de tamaño de ventana
                 NUEVO_ANCHO, NUEVA_ALTURA = event.size
                 pantalla = pygame.display.set_mode((NUEVO_ANCHO, NUEVA_ALTURA), pygame.RESIZABLE,pygame.OPENGL)# Cambia el tamaño de la ventana
+            if event.type == pygame.KEYDOWN:
+                # JUGADOR 1
+                if event.key == pygame.K_d:
+                    angulo_n += 1
+                if event.key == pygame.K_a:
+                    angulo_n -= 1
+
+        angulo = (math.radians(angulo_n))
+        x1 = tanque_1.posicion_x + 40 * math.sin(angulo)
+        y1 = tanque_1.posicion_y - 40 * math.cos(angulo)
 
         terreno_escalado = pygame.transform.scale(textura_terreno, (pantalla.get_width(), pantalla.get_height()))
         pantalla.blit(terreno_escalado, (0, 0))
 
         #Agregar tanque
-        pygame.draw.rect(pantalla,(0, 255, 0), (tanque_x, tanque_y, 50, 30))
-        if(tanque_y == ALTURA_TERRENO[tanque_x] + 70):
-            tanque_y += 0
+        pygame.draw.rect(pantalla,(0, 255, 0), (tanque_1.posicion_x, tanque_1.posicion_y, 50, 30))
+        pygame.draw.line(pantalla, (150, 150, 150),(tanque_1.posicion_x, tanque_1.posicion_y), (x1, y1), 10)
+        print(angulo, "-", x1)
+        if(tanque_1.posicion_y == ALTURA_TERRENO[tanque_1.posicion_x] + 100):
+            tanque_1.posicion_y += 0
         else:
-            tanque_y += gravedad
+            tanque_1.posicion_y += gravedad
 
         pygame.display.flip()
-
         # Limita los FPS a 60
         reloj.tick(60)
 
