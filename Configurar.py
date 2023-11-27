94fb992b6217d73ff0bc8bc786ac6b413dd5d0f8
mario <mario.jorquera.11@gmail.com>
2023-11-27 14:14:18 -0300

    Diseño Ajustes

--- Configurar.py
+++ Configurar.py
@@ -19,12 +19,16 @@ class Configurar:
         ancho_botón, altura_botón = 0.10, 0.04
         margin_ratio = 0.05  # Margen de separación entre botones
         Volver = Boton(0.36 + ancho_botón / 2, 0.75 + 0.5 * altura_botón + margin_ratio, 0.18, 0.10, "Volver", constantes.BLANCO, constantes.CELESTE, constantes.NEGRO, 50)
-        boton_dimenciones = [Boton(0.8 + ancho_botón / 2, 0.5 - 0.5 * altura_botón + margin_ratio, ancho_botón, altura_botón, "->", constantes.BLANCO, constantes.CELESTE, constantes.NEGRO, 50),
-                            Boton(0.5 + ancho_botón / 2, 0.5 - 0.5 * altura_botón + margin_ratio, ancho_botón, altura_botón, "<-", constantes.BLANCO, constantes.CELESTE, constantes.NEGRO, 50),
+        boton_dimenciones = [Boton(0.69 + ancho_botón / 2, 0.33 - 0.5 * altura_botón + margin_ratio, ancho_botón, altura_botón, ">", constantes.BLANCO, constantes.CELESTE, constantes.NEGRO, 50),
+                            Boton(0.45 + ancho_botón / 2, 0.33 - 0.5 * altura_botón + margin_ratio, ancho_botón, altura_botón, "<", constantes.BLANCO, constantes.CELESTE, constantes.NEGRO, 50),
                              Boton(0.05 + ancho_botón / 2, 0.33 - 0.5 * altura_botón + margin_ratio, ancho_botón, altura_botón, "<",constantes.BLANCO, constantes.CELESTE, constantes.NEGRO, 50),
                              Boton(0.25 + ancho_botón / 2, 0.33 - 0.5 * altura_botón + margin_ratio, ancho_botón, altura_botón, ">",constantes.BLANCO, constantes.CELESTE, constantes.NEGRO, 50),
                              Boton(0.05 + ancho_botón / 2, 0.48 - 0.5 * altura_botón + margin_ratio, ancho_botón,altura_botón, "<", constantes.BLANCO, constantes.CELESTE, constantes.NEGRO, 50),
-                             Boton(0.25 + ancho_botón / 2, 0.48 - 0.5 * altura_botón + margin_ratio, ancho_botón,altura_botón, ">", constantes.BLANCO, constantes.CELESTE, constantes.NEGRO, 50)
+                             Boton(0.25 + ancho_botón / 2, 0.48 - 0.5 * altura_botón + margin_ratio, ancho_botón,altura_botón, ">", constantes.BLANCO, constantes.CELESTE, constantes.NEGRO, 50),
+                             Boton(0.05 + ancho_botón / 2, 0.63 - 0.5 * altura_botón + margin_ratio, ancho_botón,altura_botón, "<", constantes.BLANCO, constantes.CELESTE, constantes.NEGRO, 50),
+                             Boton(0.25 + ancho_botón / 2, 0.63 - 0.5 * altura_botón + margin_ratio, ancho_botón,altura_botón, ">", constantes.BLANCO, constantes.CELESTE, constantes.NEGRO, 50),
+                             Boton(0.45 + ancho_botón / 2, 0.48 - 0.5 * altura_botón + margin_ratio, ancho_botón,altura_botón, "<", constantes.BLANCO, constantes.CELESTE, constantes.NEGRO, 50),
+                             Boton(0.69 + ancho_botón / 2, 0.48 - 0.5 * altura_botón + margin_ratio, ancho_botón,altura_botón, ">", constantes.BLANCO, constantes.CELESTE, constantes.NEGRO, 50)
                              ]
         while en_config:
             mouse = pygame.mouse.get_pos()
@@ -39,32 +43,15 @@ class Configurar:
             keys = pygame.key.get_pressed()
 
             current_time = pygame.time.get_ticks()
-            if current_time - last_change_time > change_delay:
-                # Lógica para ajustar las opciones según las teclas presionadas
-                if keys[pygame.K_UP]:
-                    # Aumenta el número de jugadores (limitado a 6)
-                    constantes.NUM_JUGADORES = min(constantes.NUM_JUGADORES + 1, 6)
-                elif keys[pygame.K_DOWN]:
-                    # Disminuye el número de jugadores (mínimo 2)
-                    constantes.NUM_JUGADORES = max(constantes.NUM_JUGADORES - 1, 2)
-                elif keys[pygame.K_RIGHT]:
-                    # Aumenta el número de partidos
-                    constantes.NUM_PARTIDAS = min(constantes.NUM_PARTIDAS + 1, 20)
-                elif keys[pygame.K_LEFT]:
-                    # Disminuye el número de partidos (mínimo 1)
-                    constantes.NUM_PARTIDAS = max(constantes.NUM_PARTIDAS - 1, 1)
-                elif keys[pygame.K_e]:
-                    # Activa/desactiva los efectos del entorno
-                    constantes.EFECTOS_ENTORNO = not constantes.EFECTOS_ENTORNO
 
-                last_change_time = current_time
+
+            last_change_time = current_time
             if keys[pygame.K_RETURN]:
                 Configuracion(constantes.NUM_JUGADORES,800,800,constantes.NUM_PARTIDAS)
             self.pantalla.fill((208, 237, 250))
             for boton in boton_dimenciones:
                 boton.dibujar(self.pantalla, constantes.ANCHO_VENTANA, constantes.ALTO_VENTANA)
             Volver.dibujar(self.pantalla, constantes.ANCHO_VENTANA, constantes.ALTO_VENTANA)
-            Escribir.render_text(self.pantalla, str(constantes.DIMENSIONES),(0.7 + ancho_botón / 2, 0.5 - 0.5 * altura_botón + margin_ratio), 30, constantes.NEGRO,"Arial")
             Escribir.render_text(self.pantalla, "Ajustes del juego", (0.47 + ancho_botón / 2, 0.07 - 0.5 * altura_botón + margin_ratio), 60, constantes.NEGRO, "More Sugar")
             # Muestra las opciones y valores en la pantalla
             Escribir.render_text(self.pantalla, "Jugadores", (0.2 + ancho_botón / 2, 0.28 - 0.5 * altura_botón + margin_ratio), 20, constantes.NEGRO, None)
@@ -74,7 +61,10 @@ class Configurar:
             Escribir.render_text(self.pantalla, "CPU",(0.2 + ancho_botón / 2, 0.57 - 0.5 * altura_botón + margin_ratio), 20,constantes.NEGRO, None)
             Escribir.render_text(self.pantalla, "0",(0.2 + ancho_botón / 2, 0.64 - 0.57 * altura_botón + margin_ratio), 20,constantes.NEGRO, None)
             #Escribir.render_text(self.pantalla, f"Efectos de Entorno: {'Activado' if constantes.EFECTOS_ENTORNO else 'Desactivado'}", (0.08 + ancho_botón / 2, 0.36 - 0.5 * altura_botón + margin_ratio), 20, constantes.NEGRO, None)
-            #Escribir.render_text(self.pantalla, f"Dimensiones: {constantes.DIMENSIONES[0]},{constantes.DIMENSIONES[1]}", (0.06 + ancho_botón / 2, 0.4 - 0.5 * altura_botón + margin_ratio), 20, constantes.NEGRO, None)
+            Escribir.render_text(self.pantalla,"Resolucion",(0.62 + ancho_botón / 2, 0.28 - 0.5 * altura_botón + margin_ratio), 20,constantes.NEGRO, None)
+            Escribir.render_text(self.pantalla, str(constantes.DIMENSIONES),(0.62 + ancho_botón / 2, 0.35 - 0.5 * altura_botón + margin_ratio), 20, constantes.NEGRO,"Arial")
+            Escribir.render_text(self.pantalla, "Efectos de Entorno",(0.62 + ancho_botón / 2, 0.43 - 0.5 * altura_botón + margin_ratio), 20,constantes.NEGRO, None)
+            Escribir.render_text(self.pantalla,f"{'Activado' if constantes.EFECTOS_ENTORNO else 'Desactivado'}",(0.62 + ancho_botón / 2, 0.50 - 0.5 * altura_botón + margin_ratio), 20,constantes.NEGRO, None)
             pygame.display.update()
             for event in pygame.event.get():
                 if event.type == pygame.MOUSEBUTTONDOWN:
@@ -89,6 +79,24 @@ class Configurar:
                     if boton_dimenciones[0].si_clic(constantes.ANCHO_VENTANA,constantes.ALTO_VENTANA):
                         constantes.DIMENSIONES[0] = int(constantes.config_maximas.ancho_pantalla)
                         constantes.DIMENSIONES[1] = int(constantes.config_maximas.alto_pantalla)
+                    if boton_dimenciones[2].si_clic(constantes.ANCHO_VENTANA,constantes.ALTO_VENTANA):
+                        constantes.NUM_JUGADORES = max(constantes.NUM_JUGADORES - 1, 2)
+                    if boton_dimenciones[3].si_clic(constantes.ANCHO_VENTANA,constantes.ALTO_VENTANA):
+                        constantes.NUM_JUGADORES = min(constantes.NUM_JUGADORES + 1, 6)
+                    if boton_dimenciones[4].si_clic(constantes.ANCHO_VENTANA,constantes.ALTO_VENTANA):
+                        constantes.NUM_PARTIDAS = max(constantes.NUM_PARTIDAS - 1, 1)
+                    if boton_dimenciones[5].si_clic(constantes.ANCHO_VENTANA,constantes.ALTO_VENTANA):
+                        constantes.NUM_PARTIDAS = min(constantes.NUM_PARTIDAS + 1, 20)
+                    if boton_dimenciones[6].si_clic(constantes.ANCHO_VENTANA,constantes.ALTO_VENTANA):
+                        constantes.NUM_CPU = max(constantes.NUM_CPU - 1, 0)
+                    if boton_dimenciones[7].si_clic(constantes.ANCHO_VENTANA,constantes.ALTO_VENTANA):
+                        constantes.NUM_CPU = min(constantes.NUM_CPU + 1, 6)
+                    if boton_dimenciones[8].si_clic(constantes.ANCHO_VENTANA, constantes.ALTO_VENTANA):
+                        constantes.EFECTOS_ENTORNO = not constantes.EFECTOS_ENTORNO
+                    if boton_dimenciones[9].si_clic(constantes.ANCHO_VENTANA, constantes.ALTO_VENTANA):
+                        constantes.EFECTOS_ENTORNO = not constantes.EFECTOS_ENTORNO
+
+
 
             pygame.display.update()
             pygame.display.flip()
